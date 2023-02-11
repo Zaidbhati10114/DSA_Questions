e7aaa228bba09c64cbbe06430cc7d8f1dd9e609b
Zaid Bhati <59171704+Zaidbhati10114@users.noreply.github.com>
2023-02-11 11:35:12 +0000

    Unique Number Question with code added

---
+++ .ccls-cache/@home@runner@11FebQuestions/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findUnique(vector<int> arr) {
+  int ans = 0;
+
+  for (int i = 0;i<arr.size();i++) {
+    ans = ans ^ arr[i];
+  }
+  return ans;
+}
+
+
+
+
+
+int main() {
+  // FIND UNIQUE ELEMENT
+
+  
+  int n;
+  cout << "Enter the size of array" << endl;
+  cin >> n;
+
+  vector<int> arr(n);
+  cout << "Enter the elements" << endl;
+  // Taking input 
+
+  for (int i=0;i<arr.size();i++) {
+    cin >> arr[i];
+    
+  }
+
+  int uniqueElement = findUnique(arr);
+
+  cout << "Unique Elemnent is : " << uniqueElement << endl;
+
+  return 0;
+
+
+
+  }
+
+   
+
