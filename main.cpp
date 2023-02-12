63cd80ed463b42843b88712d4fbe19e364d4671d
Zaid Bhati <59171704+Zaidbhati10114@users.noreply.github.com>
2023-02-12 10:25:21 +0000

    Pair Sum Code

--- main.cpp
+++ main.cpp
@@ -9,32 +9,24 @@ using namespace std;
 
 
 int main() {
-  // Intersection OF TWO ARRAYS
+  //PAIR SUM PROBLEM
 
-  vector<int> arr{1,2,3,4,6,8};
-  
-  vector<int> brr{8,4,10};
+  vector<int> arr{10,20,30,40,50};
+  int sum;
+  cout << "Enter the sum which you want to find" << endl;
+  cin >> sum;
 
-  vector<int> ans;
+  // Print all pairs of the array 
 
-  // Outer loop 
-
-  for (int i =0;i<arr.size();i++) {
-    int element = arr[i];
-    // For every single element check it is equal to the each number in second array 
-    for (int j= 0;j<brr.size();j++) {
-      if(element == brr[j]) {
-        // if element are double we have to mark it checked and replace the number
-        brr[j] = -1;
-        ans.push_back(element);
-      }
+  for (int i=0;i<arr.size();i++) {
+     int element = arr[i];
+    // for each element pair it with forward numbers in the array 
+    for (int j=i+1;j<arr.size();j++) {
+      if (element + arr[j] == sum)
+      cout << "Pair are" << " " << "(" << element << "," << arr[j] << ")" << endl;
     }
   }
-
-  // Print ans 
-  for(auto value:ans) {
-    cout << value << "" << endl;
-  }
+  
   
 
 
